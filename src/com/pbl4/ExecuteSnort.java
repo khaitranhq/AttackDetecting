9803c3097a9804b895df0748417a6b5acc9ce6c0
lioaslan <arey.tieuleo.5.7@gmail.com>
2020-12-25 21:40:25 +0700

    ok

--- src/com/pbl4/ExecuteSnort.java
+++ src/com/pbl4/ExecuteSnort.java
@@ -5,10 +5,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-public class Test {
+public class ExecuteSnort {
 	public static void main(String[] args) {
+		Main main = new Main();
+		main.setVisible(true);
+		int cnt = 0;
 		try {
-			ProcessBuilder builder = new ProcessBuilder("/bin/sh", "-c", "sudo snort -A console -i ens33 -c /etc/snort/snort.conf");
+			String execString = "sudo snort -A console -i enp0s3 -u snort -g snort -c /etc/snort/snort.conf";
+			ProcessBuilder builder = new ProcessBuilder("/bin/sh", "-c", execString);
 			builder.redirectErrorStream(true);
 			Process process = builder.start();
 			InputStream is = process.getInputStream();
@@ -16,9 +20,11 @@ public class Test {
 
 			String line = null;
 			while ((line = reader.readLine()) != null) {
-			   System.out.println(line);
+//				System.out.println(line);
+				if (line.contains("Possible DoS Attack"))
+					main.appendText(line);
 			}
-		} catch (IOException e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
--- src/com/pbl4/Main.java
+++ src/com/pbl4/Main.java
@@ -1,30 +1,45 @@
 package com.pbl4;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextPane;
+import javax.swing.JTextArea;
+import java.awt.List;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
 
 public class Main extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-
+	private JTextArea txtrAAA;
+	private JScrollPane scrollPane;
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-//		EventQueue.invokeLater(new Runnable() {
-//			public void run() {
-//				try {
-//					Main frame = new Main();
-//					frame.setVisible(true);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main frame = new Main();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	protected void appendText(String s) {
+		String currentText = txtrAAA.getText();
+		currentText = currentText + "\n" + s;
+		txtrAAA.setText(currentText);
 	}
 
 	/**
@@ -32,11 +47,17 @@ public class Main extends JFrame {
 	 */
 	public Main() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 715, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(12, 12, 681, 246);
+		contentPane.add(scrollPane);
+		
+		txtrAAA = new JTextArea();
+		scrollPane.setViewportView(txtrAAA);
 	}
-
 }
